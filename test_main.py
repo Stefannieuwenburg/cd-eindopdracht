f5088c5632117a0aa362f7845cc94da312a17eff
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-02-10 18:29:49 +0100

    commit

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
-import app
+import main
 
 def test_index():
-    assert app.index() == f"<h1>Dit was de laatste opdracht<h1>"
+    assert main.index() == f"<h1>Dit was de laatste opdracht<h1>"
 
 def test_Stefan():
-    assert app.Stefan() == f"<h1>Goed Bezig Stefan!<h1>"
+    assert main.Stefan() == f"<h1>Goed Bezig Stefan!<h1>"
